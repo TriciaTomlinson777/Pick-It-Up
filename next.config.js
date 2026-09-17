5c2051e6ceedc00305aa25cf83126cade0f4074a
TriciaTomlinson777 <triciatomlinson777@gmail.com>
2026-09-17 00:40:24 +0000

    Harden Rekognition photo moderation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    '@aws-sdk/client-comprehend',
+    '@aws-sdk/client-rekognition',
+    'sharp',
+  ],
   images: {
     remotePatterns: [
       {
